158cc03e1f382752c5d6f6afbc0f68a56abb12b1
j446610313 <49707082+j446610313@users.noreply.github.com>
2024-08-04 22:19:10 +0800

    Delete ide-helper directory

--- ide-helper/libraries/memory.lua
+++
@@ -1,6 +0,0 @@
----@class memory
----@field public new fun(type: string, n: number): any @ Creates a new C-type array of the specified type and size.
--- Valid types: 'char', 'unsigned char', 'short', 'unsigned short', 'int', 'unsigned int', 'long long', 'unsigned long long', 'float'
-
----@type memory
-_G.memory = {}
